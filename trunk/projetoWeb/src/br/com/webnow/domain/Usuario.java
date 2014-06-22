2fd81fddcaa19348268c51e6df2d15c311d6c8ab
wpnfortini@gmail.com <wpnfortini@gmail.com@3c474b41-8f4d-b7d7-56d3-83c8351592e6>
2014-06-22 18:22:24 +0000

    wellington: version 1

---
+++ trunk/projetoWeb/src/br/com/webnow/domain/Usuario.java
@@ -0,0 +1,81 @@
+package br.com.webnow.domain;
+
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.Indexed;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+
+
+@NodeEntity
+public class Usuario {
+	
+	@GraphId
+	private Long nodeId;
+	
+	@Indexed
+	private String login;
+	
+	private String password;
+	
+	@Indexed
+	private String nome;
+	private String apelido;
+	private String fotoPerfil;
+	public Long getNodeId() {
+		return nodeId;
+	}
+	public void setNodeId(Long nodeId) {
+		this.nodeId = nodeId;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getApelido() {
+		return apelido;
+	}
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+	
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Usuario user = (Usuario) o;
+        if (nodeId == null) return super.equals(o);
+        return nodeId.equals(user.nodeId);
+
+    }
+	
+	@Override
+    public int hashCode() {
+
+        return nodeId != null ? nodeId.hashCode() : super.hashCode();
+    }
+	
+	
+
+}
