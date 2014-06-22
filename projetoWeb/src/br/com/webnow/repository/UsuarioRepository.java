b58177329d15cb17938713688ee7eab2a579641b
wpnfortini <wpnfortini@gmail.com>
2014-06-22 18:22:24 +0000

    wellington: version 1

---
+++ projetoWeb/src/br/com/webnow/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.webnow.repository;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+import br.com.webnow.domain.Usuario;
+
+public interface UsuarioRepository extends GraphRepository<Usuario>,
+                                           ViuUserDetailsService{
+
+}
