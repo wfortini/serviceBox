3fba3346c5e298c7a7aa658ee9ccf758345581af
wellington <wpnfortini@gmail.com>
2015-07-05 16:40:36 +0000

    alteração para usar rest

--- projetoWeb/src/br/com/webnow/geo/spatial/Neo4jRestClientFactory.java
+++ projetoWeb/src/br/com/webnow/geo/spatial/Neo4jRestClientFactory.java
@@ -5,6 +5,7 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
