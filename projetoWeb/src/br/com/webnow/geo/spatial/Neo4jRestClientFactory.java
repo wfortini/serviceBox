e60eee0577424554e5cc1c43efd2aea7924473c8
wellington <wpnfortini@gmail.com>
2015-07-05 15:42:04 +0000

    teste

---
+++ projetoWeb/src/br/com/webnow/geo/spatial/Neo4jRestClientFactory.java
@@ -0,0 +1,51 @@
+package br.com.webnow.geo.spatial;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+public class Neo4jRestClientFactory {
+	
+	
+	private static String databaseUser;
+
+	  private static String databasePassword;
+
+	  @Value("${database_usr}")
+	  public void setDatabaseUser(String databaseUser)
+	  {
+	    Neo4jRestClientFactory.databaseUser = databaseUser;
+	  }
+
+	  @Value("${database_pwd}")
+	  public void setDatabasePassword(String databasePassword)
+	  {
+	    Neo4jRestClientFactory.databasePassword = databasePassword;
+	  }
+
+	  public static RestTemplate getInstance()
+	  {
+	    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+
+	    credsProvider.setCredentials(
+	            new AuthScope(null, -1),
+	            new UsernamePasswordCredentials(databaseUser, databasePassword));
+
+	    HttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+
+	    RestTemplate restTemplate = new RestTemplate();
+
+	    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+	    return restTemplate;
+	  }
+
+}
---
+++ projetoWeb/src/br/com/webnow/geo/spatial/SpatialPluginRestClient.java
@@ -0,0 +1,5 @@
+package br.com.webnow.geo.spatial;
+
+public class SpatialPluginRestClient {
+
+}
