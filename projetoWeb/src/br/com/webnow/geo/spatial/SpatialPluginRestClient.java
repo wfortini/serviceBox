1c0dc1541d2d3dd362fb5ce4238fa2c2cead0037
wellington <wpnfortini@gmail.com>
2015-07-05 19:15:41 +0000

    we

--- projetoWeb/src/br/com/webnow/geo/spatial/SpatialPluginRestClient.java
+++ projetoWeb/src/br/com/webnow/geo/spatial/SpatialPluginRestClient.java
@@ -1,5 +1,24 @@
 package br.com.webnow.geo.spatial;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class SpatialPluginRestClient {
+	
+	@Value("${database_url}")
+	private String databaseURL;
+	 
+	public String findPlugin(){
+		
+		RestTemplate rest = Neo4jRestClientFactory.getInstance();
+		
+		String url = databaseURL + "/db/data/ext/SpatialPlugin";
+		
+		String response = rest.getForObject(databaseURL, String.class);
+		
+		return response;
+	}
 
 }
